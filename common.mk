1b98eed1c82136265387c5b3554b62ead1020859
Harish <sanganiharish263@gmail.com>
2023-11-25 18:52:15 -0600

    sm8150-common: Append build time to zip name
    Signed-off-by: Harish <sanganiharish263@gmail.com>

--- common.mk
+++ common.mk
@@ -18,6 +18,9 @@ $(call inherit-product, $(LOCAL_PATH)/interfaces.mk)
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Derp versioning
+DERP_VERSION_APPEND_TIME_OF_DAY := true
+
 # Get Qcom components
 TARGET_EXCLUDE_QCOM_SEPOLICY := true
 TARGET_BOARD_PLATFORM := msmnile
