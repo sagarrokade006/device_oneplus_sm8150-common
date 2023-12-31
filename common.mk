7053bdc5784550b72c3da7939dc60e342773627b
Harish <sanganiharish263@gmail.com>
2023-12-30 18:53:34 -0600

    Init: Derp
    Signed-off-by: Harish <sanganiharish263@gmail.com>

--- common.mk
+++ common.mk
@@ -33,7 +33,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-yaap
+    $(LOCAL_PATH)/overlay-derp
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
@@ -109,7 +109,7 @@ PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.dsi.ant@1.0.vendor
 
-# YAAP Device Settings
+# DERP Device Settings
 PRODUCT_PACKAGES += \
     DeviceSettings
 
